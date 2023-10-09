f30c865a64eb5b8fa1b07af8c7785d65a1aa73cb
Alex McMullen <alex12601@yahoo.com>
2023-10-09 23:39:47 +0000

    added imports to repositories

--- src/main/java/com/example/repository/AccountRepository.java
+++ src/main/java/com/example/repository/AccountRepository.java
@@ -1,4 +1,20 @@
 package com.example.repository;
 
-public interface AccountRepository {
+import com.example.entity.Account;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+public interface AccountRepository extends JpaRepository<Account, Long>
+{
+    /**
+     * Retrieve an account by its username and password
+     * 
+     * @param username, the username of the account to be retrieved
+     * @param password, the password of the account to be retrieved
+     * @return the retrieved account
+     */
+    @Query("from Account where username = :userName and password = :password")
+    Account logInAccount(@Param("userName") String username, @Param("password") String password);
 }
